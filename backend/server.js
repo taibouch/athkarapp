661a1c2e05ee3d94b400798b1d7057bf5ba5de53
taibouch <zakariakebir7@gmail.com>
2026-03-30 04:09:26 -0700

    server code set

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,12 @@ mongoose.connect(process.env.MONGODB_URI)
 app.get('/', (req, res) => {
   res.send('Backend is running!');
 });
+app.get('/home', (req, res) => {
+  res.send('<h1>Home</h1>');
+});
+app.get('/seler', (req, res) => {
+  res.send('<h1>seler</h1>');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
